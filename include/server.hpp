aeff83646598e19ca4ca032d53a03c408978be9e
ozcantolgahan <tolgahanozcan@hotmail.com>
2024-07-02 21:58:54 +0300

    Add sample log and usernames controls in server

--- include/server.hpp
+++ include/server.hpp
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <mutex>
+#include <string>
+#include <unordered_map>
 
 class Server
 {
@@ -14,9 +16,12 @@ private:
     int server_socket;
     int port;
     std::vector<int> clients;
+    std::unordered_map<int, std::string> usernames;
     std::mutex clients_mutex;
 
     void handle_client(int client_socket);
+    void log_message(const std::string &message);
+    std::string get_timestamp();
 };
 
-#endif // SERVER_HPP
+#endif
